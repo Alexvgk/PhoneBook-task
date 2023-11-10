ed852c6a86c0bf007603badd550eeb2c6278aa1b
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-11-10 23:58:03 +0300

    added data transfer object for contanct

---
+++ DTO/DtoContact.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneBook_task.DTO
+{
+    public class DtoContact
+    {
+        public string? Name { get; set; }
+        public string? MobilePhone { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Data { get; set; }
+    }
+}
