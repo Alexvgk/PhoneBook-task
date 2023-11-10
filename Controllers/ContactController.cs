700879b065b526a4790e11010d9d7b65095e6a2c
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-11-10 21:08:44 +0300

    initial controller

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook_task.Models;
+using System.Diagnostics;
+
+namespace PhoneBook_task.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
