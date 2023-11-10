52b44c0909db2310ad5ae1658f54cb4216c771e6
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-11-10 23:55:46 +0300

    added DdContext for EF and Automapper

---
+++ Services/ContactDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook_task.Models;
+
+namespace PhoneBook_task.Services
+{
+    public class ContactDbContext : DbContext
+    {
+        public ContactDbContext(DbContextOptions<ContactDbContext> options) :base(options)
+        {
+            Database.EnsureDeleted();  
+            Database.EnsureCreated();   
+        }
+
+        public DbSet<Contact> Contacts { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Services/ContactsMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PhoneBook_task.DTO;
+using PhoneBook_task.Models;
+using System;
+
+namespace PhoneBook_task.Services
+{
+    public class ContactsMappingProfile : Profile
+    {
+        public ContactsMappingProfile()
+        {
+            CreateMap<DtoContact, Contact>();
+        }
+    }
+}
