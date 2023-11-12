b63ff1ac4dee7169ece40445cc509797edb32a24
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-11-12 03:53:56 +0300

    added DI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using PhoneBook_task.Models.BaseModel;
+using PhoneBook_task.Repository;
+using PhoneBook_task.Services;
+using Microsoft.EntityFrameworkCore;
+using PhoneBook_task.Models;
+using Microsoft.AspNetCore.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(ContactsMappingProfile));
+builder.Services.AddDbContext<ContactDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Scoped);
+builder.Services.AddScoped<IBaseRepository<Contact>, BaseRepository<Contact>>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
