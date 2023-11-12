2eb8cdc1ab17725c93dfb8a0d9fe0d76d57aefe4
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-11-12 03:57:30 +0300

    added some methods for controller like delete and etc.

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook_task.DTO;
 using PhoneBook_task.Models;
+using PhoneBook_task.Models.BaseModel;
+using PhoneBook_task.Repository;
 using System.Diagnostics;
 
 namespace PhoneBook_task.Controllers
@@ -8,19 +11,58 @@ namespace PhoneBook_task.Controllers
     {
         private readonly ILogger<ContactController> _logger;
 
-        public ContactController(ILogger<ContactController> logger)
+        private IBaseRepository<Contact> Repo { get; set; }
+
+        public ContactController(ILogger<ContactController> logger, IBaseRepository<Contact> baseData)
         {
+            Repo = baseData;
             _logger = logger;
         }
 
         public IActionResult All()
         {
-            return View();
+            var contList = Repo.getData();
+            return View(contList.Result);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] DtoContact cont)
+        {
+            try
+            {
+                var result = await Repo.CreateContact(cont);
+
+                if (result)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Failed to create contact");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Обработка других исключений, если необходимо
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+            public Contact GetContactById(int id) {
+            var contact = Repo.GetDataById(id);
+            return contact.Result;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            await Repo.DeleteContact(id);
+            return Ok(new { id });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
